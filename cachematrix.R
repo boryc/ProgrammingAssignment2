940a7f583b39de46211567ec402fb03f48622fc2
boryc <lborycki@gmail.com>
2017-09-06 20:51:08 +0200

    Matrix manipulating functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions, that use closures to cache inversions of the matrix
 
-## Write a short comment describing this function
+## Function that creates vector, that consists of functions manipulating the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
+  invertedMtrx <- NULL
+  
+  setMtrx <- function(newMtrx) {
+    x <<- newMtrx
+    invertedMtrx <<- NULL
+  }
+  
+  getMtrx <- function() {
+    x
+  } 
+  
+  setInvertedMatrix <- function(newInvertedMtrx) {
+    invertedMtrx <<- newInvertedMtrx
+  }
+  
+  getInvertedMtrx <- function() {
+    invertedMtrx
+  }
+  
+  list(setMtrx = setMtrx, getMtrx = getMtrx,
+       setInvertedMatrix = setInvertedMatrix,
+       getInvertedMtrx = getInvertedMtrx)
 }
 
 
-## Write a short comment describing this function
+## Function that uses makeCacheMatrix vector with functions to manipulate matrices,
+##   calculates matrix inversions and puts it in cache for future reference
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invertedMtrx <- x$getInvertedMtrx()
+  
+  if(!is.null(invertedMtrx)) {
+    message("getting cached data")
+    return(invertedMtrx)
+  }
+  
+  mtrx <- x$getMtrx()
+  invertedMtrx <- solve(mtrx) %*% mtrx
+  x$setInvertedMatrix(invertedMtrx)
+  invertedMtrx
 }
